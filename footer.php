dfa9cf2e4cbf88ef952033952c60bb79fa9bc87e
Fritz <fre.healy@gmail.com>
2018-02-09 13:23:53 -0500

    major templating header and footer

--- footer.php
+++ footer.php
@@ -14,7 +14,27 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-
+		<div class="wrapper cap">
+			<?php $facebook_link = get_field("facebook_link");
+			$linkedin_link = get_field("linkedin_link");
+			$instagram_link = get_field("instagram_link");
+			if($facebook_link||$instagram_link||$linkedin_link):?>
+				<div class="row-1 social">
+					<?php if($instagram_link):?>
+						<a href="<?php echo $instagram_link;?>"><i class="fa fa-instagram"></i></a>
+					<?php endif;?>
+					<?php if($linkedin_link):?>
+						<a href="<?php echo $linkedin_link;?>"><i class="fa fa-linkedin"></i></a>
+					<?php endif;?>
+					<?php if($facebook_link):?>
+						<a href="<?php echo $facebook_link;?>"><i class="fa fa-facebook"></i></a>
+					<?php endif;?>
+				</div><!--.row-1-->
+			<?php endif;?>
+			<div class="row-2">
+				<?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>
+			</div><!--.row-2-->
+		</div><!--.wrapper.cap-->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
--- header.php
+++ header.php
@@ -25,22 +25,38 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'acstarter' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="wrapper">
-			
+		<div class="wrapper cap">
 			<?php if(is_home()) { ?>
-	            <h1 class="logo">
-	            <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
+	            <h1 class="logo col-1">
+	            <a href="<?php bloginfo('url'); ?>"><img src="<?php echo get_template_directory_uri()."/images/logo.png";?>" alt="<?php bloginfo('name'); ?>"></a>
 	            </h1>
 	        <?php } else { ?>
-	            <div class="logo">
-	            <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
+	            <div class="logo col-2">
+	            <a href="<?php bloginfo('url'); ?>"><img src="<?php echo get_template_directory_uri()."/images/logo.png";?>" alt="<?php bloginfo('name'); ?>"></a>
 	            </div>
 	        <?php } ?>
-
-			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'MENU', 'acstarter' ); ?></button>
-				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-			</nav><!-- #site-navigation -->
+			<div class="col-2">
+				<?php $facebook_link = get_field("facebook_link");
+				$linkedin_link = get_field("linkedin_link");
+				$instagram_link = get_field("instagram_link");
+				if($facebook_link||$instagram_link||$linkedin_link):?>
+					<div class="row-1 social">
+						<?php if($instagram_link):?>
+							<a href="<?php echo $instagram_link;?>"><i class="fa fa-instagram"></i></a>
+						<?php endif;?>
+						<?php if($linkedin_link):?>
+							<a href="<?php echo $linkedin_link;?>"><i class="fa fa-linkedin"></i></a>
+						<?php endif;?>
+						<?php if($facebook_link):?>
+							<a href="<?php echo $facebook_link;?>"><i class="fa fa-facebook"></i></a>
+						<?php endif;?>
+					</div><!--.row-1-->
+				<?php endif;?>
+				<nav id="site-navigation" class="main-navigation row-2" role="navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'MENU', 'acstarter' ); ?></button>
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+				</nav><!-- #site-navigation -->
+			</div><!--col-2-->
 	</div><!-- wrapper -->
 	</header><!-- #masthead -->
 
