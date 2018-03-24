64d0a92214cc8763ca45f031adad7d9491e43e90
Austin Crane <austincrane@gmail.com>
2018-03-24 10:12:06 -0400

    ga tracking code

--- footer.php
+++ footer.php
@@ -41,6 +41,15 @@
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-116345592-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-116345592-1');
+</script>
 
 </body>
 </html>
